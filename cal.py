f094fc3d702b36324540caae70b069add307439b
Yadnyesh Shingade <yadneshshingade@gmail.com>
2025-08-26 15:05:42 +0530

    add implemented

---
+++ cal.py
@@ -0,0 +1,10 @@
+def addition(a,b):
+    add= a + b
+    return add
+
+
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+
+sum = addition(a,b)
+print(f"Sum is : {sum}")
